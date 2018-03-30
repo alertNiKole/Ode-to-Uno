2d363590cf84946859d73dc65069e13e1ac101a4
NiKole Maxwell <nvymaxwell@outlook.com>
2018-03-29 22:44:10 -0400

    A | Completed raw style

--- src/scripts/uno.js
+++ src/scripts/uno.js
@@ -3,3 +3,9 @@
 let rng =function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
   }
+/*
+  - orange - #d26227;
+  - pink    -#c71562;
+  - red     - #c71515;
+  - purple - #663399,
+  - Black  - #000000 */
